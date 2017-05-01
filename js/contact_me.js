e29e6bb2f9d30d1d0d28f389e6d8ac0ad68bf5d6
xonix <xonixx@gmail.com>
2017-05-01 16:25:37 +0300

    rfct

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/info@cmlteam.com",
+                url: $('#contactForm').attr('action'),
                 dataType: "json",
                 type: "POST",
                 data: {
