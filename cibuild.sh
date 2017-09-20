a8473aeb36b91c9c09aade36e8ad662916ac080b
vgubarkov <xonixx@gmail.com>
2017-09-20 22:02:36 +0300

    deploy to our server with https

--- cibuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # halt script on error
-
-bundle exec jekyll build
-mv _site site # looks like deploy phase doesn't like underscores
-echo www.cmlteam.com > site/CNAME # custom domain
