2157df337aae3058c87b33a7517a20b1c65b69e8
vgubarkov <xonixx@gmail.com>
2017-09-20 23:08:07 +0300

    deploy to our server with https

---
+++ .d.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -v
+ssh -i /tmp/deploy_rsa apps1@prod.cmlteam.com "(cd ; echo \"\$(date) www.cmlteam.com \$(pwd)\" ; rm -rf www.cmlteam.com ; mkdir www.cmlteam.com) >> /tmp/.deploy 2>&1"
+cd _site ; scp -i /tmp/deploy_rsa -C -r * apps1@prod.cmlteam.com:~/www.cmlteam.com
