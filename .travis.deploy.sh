42d39cf192bd4ac39d2ca12252477d49ee7d3999
xonix <xonixx@gmail.com>
2019-03-05 17:39:08 +0200

    change deployment config to deply to /test/

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -2,5 +2,9 @@
 
 set -v
 
-ssh -i /tmp/deploy_rsa apps1@prod.cmlteam.com "(echo \"\$(date) www.cmlteam.com \$(pwd)\" ; rm -rf www.cmlteam.com ; mkdir www.cmlteam.com) >> /tmp/.deploy 2>&1"
-cd _site ; scp -i /tmp/deploy_rsa -C -r * apps1@prod.cmlteam.com:~/www.cmlteam.com
+ssh -i /tmp/deploy_rsa apps1@prod.cmlteam.com "
+    (echo \"\$(date) www.cmlteam.com \$(pwd)\" ;
+    rm -rf www.cmlteam.com ;
+    mkdir www.cmlteam.com ;
+    mkdir www.cmlteam.com/test ) >> /tmp/.deploy 2>&1"
+cd _site ; scp -i /tmp/deploy_rsa -C -r * apps1@prod.cmlteam.com:~/www.cmlteam.com/test
