32c9a4a3ed607413cf5323e1612e3e63850fb6d4
AnnaLikhachova <a.cannsh@gmail.com>
2019-03-14 20:37:54 +0200

    Created modifiable list for service section.

--- js/service.js
+++ js/service.js
@@ -1,286 +1,166 @@
 $(function() {
 
-    // use let, we don't need global variables now.
+// use let, we don't need global variables now.
 
     let lisFirst = document.getElementsByClassName("first-section");
     let lisSecond = document.getElementsByClassName("second-section");
     let lisHidden = document.getElementsByClassName("hidden-elements-section");
 
+// going to display elements from currentElementFirst to currentElementFirst + showElementsNumber;
+// showElementsNumber is supposed to change on window resize to 2,3 or 5
+//let currentElementFirst = 0;
+//let currentElementSecond = 0;
+    let currentIndexFirst = 0;
+    let currentIndexSecond = 0;
 
-    // going to display elements from currentElementFirst to currentElementFirst + showElementsNumber;
-    // showElementsNumber is supposed to change on window resize to 2,3 or 5
-    let currentElementFirst = 0;
-    let currentElementSecond = 0;
+// check initial clientWidth and set showElementsNumber here :
+    let step = calculateStep(document.body.clientWidth);
+    showElementsFirstBlock(currentIndexFirst,  step);
+    showElementsSecondBlock(currentIndexSecond, step);
 
-    // check initial clientWidth and set showElementsNumber here :
-    let showElementsNumber; // = ...
+// create callback functions on arrows clicked
+// you dont need to change these functions on window resize. Only showElementsNumber is changed;
 
 
-    // create callback functions on arrows clicked
-    // you dont need to change these functions on window resize. Only showElementsNumber is changed;
     $(".left-button-first").on('click', function () {
-        console.log('Left first clicked!');
+            console.log('Left first clicked!');
+            if (currentIndexFirst > 0 ) {
+                currentIndexFirst -= 1;
+                showElementsFirstBlock(currentIndexFirst, step); }
+        }
+    );
 
-        if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-            console.log("720*480 currentEementFirst " + currentEementFirst);
-
-            if (currentElementFirst > 0) {   // now we start counting from zero
-                $(".first-section").css({
-                    "display": "none"
-                });
-
-                currentElementFirst -= 1;
-
-                // set display:flex on elements from [currentElementFirst] to
-                // [currentElementFirst + showElementsNumber - 1] of lisFirst
-                // look at .slice() jquery function and examples;
-
-            } else {
-                return;
+    $(".right-button-first").on('click', function () {
+            console.log('Right first clicked!');
+            if (currentIndexFirst + step < 5 ) {
+                currentIndexFirst += 1;
+                showElementsFirstBlock(currentIndexFirst, step);
             }
         }
-    });
+    );
+
+    $(".right-button-second").on('click', function () {
+            console.log('Right first clicked!');
+            if (currentIndexSecond + step < 5 ) {
+                currentIndexSecond += 1;
+                showElementsSecondBlock(currentIndexSecond, step);
+            }
+        }
+    );
+
+    $(".left-button-second").on('click', function () {
+            console.log('Left first clicked!');
+            if (currentIndexSecond > 0 ) {
+                currentIndexSecond -= 1;
+                showElementsSecondBlock(currentIndexSecond, step);
+            }
+        }
+    );
 
 
-    //other callbacks ...
+//other callbacks ...
+
+    function showElementsFirstBlock(indexElement, step){
+        console.log("parameters are"+indexElement + " "+ step);
+        $(".first-section").css({"display": "none"});
+        $(".first-section").slice(indexElement, step+indexElement).css({"display": "list-item"});
+        displayButtonsFirstBlock(indexElement, step);
+    };
+
+    function showElementsSecondBlock(indexElement, step){
+        $(".second-section").css({"display": "none"});
+        $(".second-section").slice(indexElement, step+indexElement).css({"display": "list-item"});
+        displayButtonsSecondBlock(indexElement, step);
+    };
+
+    function displayButtonsFirstBlock(currentIndexFirst, step){
+
+        if(currentIndexFirst === 0){
+            $(".left-button-first").css({
+                "display": "none"
+            });
+        }else {
+            $(".left-button-first").css({
+                "display": "flex"
+            });
+        }
+
+        if(currentIndexFirst + step === 5){
+            $(".right-button-first").css({
+                "display": "none"
+            });
+        }else {
+            $(".right-button-first").css({
+                "display": "flex"
+            });
+        }
+
+        if(step !== 5){
+            $(".hidden-elements-section").css({
+                "display": "flex"
+            });
+        }else {
+            $(".hidden-elements-section").css({
+                "display": "none"
+            });
+        }
+    }
+
+    function displayButtonsSecondBlock(currentIndexSecond, step){
+
+        if(currentIndexSecond === 0){
+            $(".left-button-second").css({
+                "display": "none"
+            });
+        }else {
+            $(".left-button-second").css({
+                "display": "flex"
+            });
+        }
+
+        if(currentIndexSecond + step === 5){
+            $(".right-button-second").css({
+                "display": "none"
+            });
+        }else {
+            $(".right-button-second").css({
+                "display": "flex"
+            });
+        }
+
+        if(step !== 5){
+            $(".hidden-elements-section").css({
+                "display": "flex"
+            });
+        }else {
+            $(".hidden-elements-section").css({
+                "display": "none"
+            });
+        }
+    }
+
+
+    function calculateStep(width){
+        if (width <= 480){
+            return 2;
+        } else if (width <= 720 && width > 480){
+            return 3;
+        } else {
+            return 5;
+        }
+    }
 
 
     $(window).resize(function () {
+        let newStep = calculateStep(document.body.clientWidth);
 
-        // set showElementsNumberNew based on new clientWidth;
-
-        // if showElementsNumberNew === showElementsNumber do nothing, leave all as is;
-
-        console.log('Current element first is', currentElementFirst);
-        console.log('Nothing to change');
-        return;
-
-        // else (i.e. if showElementsNumberNew !== showElementsNumber) set showElementsNumber = showElementsNumberNew
-        currentElementFirst = 0;
-        currentElementSecond = 0;
-        // set display:flex on elements from [0] to [showElementsNumber-1] of lisFirst and lisSecond
-        // display:none on elements from [showElementsNumber] to [4];
-
-
+        if (newStep !== step) {
+            currentIndexFirst = 0;
+            currentIndexSecond = 0;
+            step = newStep;
+            showElementsFirstBlock(currentIndexFirst, step);
+            showElementsSecondBlock(currentIndexSecond, step);
+        }
     });
 });
 
-
-//YOUR OLD CODE
-$(window).resize(function () {
-
-    return;   //doesn't work now, returns immediately
-
-    var lisFirst = document.getElementsByClassName("first-section");
-    var lisSecond = document.getElementsByClassName("second-section");
-    var lisHidden = document.getElementsByClassName("hidden-elements-section");
-    var currentEementFirst = 2;
-    var currentEementSecond = 2;
-
-    if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-        lisFirst[0].style.display = 'none';
-        lisFirst[4].style.display = 'none';
-        lisHidden[0].style.display = 'flex';
-        lisHidden[1].style.display = 'flex';
-
-        lisSecond[0].style.display = 'none';
-        lisSecond[4].style.display = 'none';
-        lisHidden[2].style.display = 'flex';
-        lisHidden[3].style.display = 'flex';
-
-    }
-
-    if (document.body.clientWidth <= 480) {
-        lisFirst[0].style.display = 'none';
-        lisFirst[3].style.display = 'none';
-        lisFirst[4].style.display = 'none';
-        lisHidden[0].style.display = 'flex';
-        lisHidden[1].style.display = 'flex';
-
-        lisSecond[0].style.display = 'none';
-        lisSecond[3].style.display = 'none';
-        lisSecond[4].style.display = 'none';
-        lisHidden[2].style.display = 'flex';
-        lisHidden[3].style.display = 'flex';
-
-
-
-    }
-
-    if (document.body.clientWidth > 720) {
-        lisHidden[0].style.display = 'none';
-        lisHidden[1].style.display = 'none';
-        lisHidden[2].style.display = 'none';
-        lisHidden[3].style.display = 'none';
-
-        lisFirst[0].style.display = 'block';
-        lisFirst[3].style.display = 'block';
-        lisFirst[4].style.display = 'block';
-        lisSecond[0].style.display = 'block';
-        lisSecond[3].style.display = 'block';
-        lisSecond[4].style.display = 'block';
-    }
-
-    moveLeftFirstBlock = () => {
-
-        if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-            console.log("720*480 currentEementFirst "+currentEementFirst);
-
-            if (currentEementFirst > 1) {
-                $(".first-section").css({
-                    "display": "none"
-                });
-                currentEementFirst -= 1;
-                lisFirst[currentEementFirst].style.display = 'block';
-                lisFirst[currentEementFirst + 1].style.display = 'block';
-                lisFirst[currentEementFirst - 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-        }
-
-        if (document.body.clientWidth <= 480) {
-            console.log("480 currentEementFirst "+currentEementFirst);
-
-            if (currentEementFirst > 1) {
-                $(".first-section").css({
-                    "display": "none"
-                });
-                currentEementFirst -= 1;
-                lisFirst[currentEementFirst].style.display = 'block';
-                lisFirst[currentEementFirst - 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-
-        }
-    }
-
-
-    moveRightFirstBlock = () => {
-
-        if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-
-            if (currentEementFirst < 3) {
-                $(".first-section").css({
-                    "display": "none"
-                });
-                currentEementFirst += 1;
-                lisFirst[currentEementFirst].style.display = 'block';
-                lisFirst[currentEementFirst + 1].style.display = 'block';
-                lisFirst[currentEementFirst - 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-        }
-
-        if (document.body.clientWidth <= 480) {
-
-            if (currentEementFirst < 3) {
-                $(".first-section").css({
-                    "display": "none"
-                });
-
-                currentEementFirst += 1;
-                lisFirst[currentEementFirst].style.display = 'block';
-                lisFirst[currentEementFirst + 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-
-
-        }
-
-    }
-
-    moveLeftSecondBlock  = () =>  {
-
-        if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-
-            if (currentEementSecond > 1) {
-
-                $(".second-section").css({
-                    "display": "none"
-                });
-
-                currentEementSecond -= 1;
-                lisSecond[currentEementSecond].style.display = 'block';
-                lisSecond[currentEementSecond + 1].style.display = 'block';
-                lisSecond[currentEementSecond - 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-        }
-
-        if (document.body.clientWidth <= 480) {
-
-            if (currentEementSecond > 1) {
-
-                $(".second-section").css({
-                    "display": "none"
-                });
-
-                currentEementSecond -= 1;
-                lisSecond[currentEementSecond].style.display = 'block';
-                lisSecond[currentEementSecond - 1].style.display = 'block';
-            } else {
-
-                return;
-            }
-
-        }
-
-    }
-
-    moveRightSecondBlock  = () =>  {
-
-        if (document.body.clientWidth <= 720 && document.body.clientWidth > 480) {
-
-            if (currentEementSecond < 3) {
-
-                $(".second-section").css({
-                    "display": "none"
-                });
-
-                currentEementSecond += 1;
-                lisSecond[currentEementSecond].style.display = 'block';
-                lisSecond[currentEementSecond + 1].style.display = 'block';
-                lisSecond[currentEementSecond - 1].style.display = 'block';
-            } else {
-
-                return;
-            }
-        }
-
-        if (document.body.clientWidth <= 480) {
-
-            if (currentEementSecond < 3) {
-
-                $(".second-section").css({
-                    "display": "none"
-                });
-
-                currentEementSecond += 1;
-                lisSecond[currentEementSecond].style.display = 'block';
-                lisSecond[currentEementSecond + 1].style.display = 'block';
-
-            } else {
-
-                return;
-            }
-
-        }
-
-    }
-
-});
