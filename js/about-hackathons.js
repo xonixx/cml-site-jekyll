7c359bb8305b84f3b45888a653dbc04039cd0977
romakatsa <romakatsa@gmail.com>
2019-03-21 01:19:20 +0200

    Fix portfolio and hackathons filters

---
+++ js/about-hackathons.js
@@ -0,0 +1,107 @@
+var shufflemeHackathons = (function ($) {
+    'use strict';
+    let $grid = $('#about-hackathons #grid'), //locate what we want to sort
+        $filterOptions = $('#about-hackathons .portfolio-sorting li'),  //locate the filter categories
+        $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
+        shuffler,
+        init = function (onInit) {
+            // None of these need to be executed synchronously
+            setTimeout(function () {
+                listenHackathons();
+                setupHackathonsFilters();
+                onInit();
+            }, 100);
+
+            // instantiate the plugin
+            shuffler = new Shuffle($grid[0], {
+                itemSelector: '[class*="col-"][data-groups]',
+                sizer: $sizer[0]
+            });
+        };
+
+        // Set up button clicks
+        var setupHackathonsFilters = function () {
+            let $btns = $filterOptions.children();
+            $btns.on('click', function (e) {
+                e.preventDefault();
+                let $this = $(this),
+                    group = $this.data('group');
+
+                // Hide current label, show current label in title
+                $('#about-hackathons .portfolio-sorting li a').removeClass('active');
+
+                $this.addClass('active');
+
+                // Filter elements
+                shuffler.filter(group);
+
+                $grid.find('li').filter(function (i, e) {
+                    let $img = $(this).find('img');
+                    $img.removeAttr('data-bp');
+                    let groups = $(e).data('groups');
+                    if (!groups)
+                        return false;
+                    let isSelectedGrp = groups.indexOf(group) >= 0;
+                    if (isSelectedGrp && $img.length) {
+                        $img.attr('data-bp', $img.attr('src').replace('Converted120', 'Converted300'));
+                    }
+                    return isSelectedGrp;
+                }).find('a')
+                    .off('click.bigpicture')
+                    .on('click.bigpicture', function (e) {
+                        e.preventDefault();
+                        BigPicture({
+                            el: e.target,
+                            gallery: '#grid'
+                        })    
+                    });
+            });
+
+            $btns = null;
+        },
+
+        // Re layout shuffle when images load. This is only needed
+        // below 768 pixels because the .picture-item height is auto and therefore
+        // the height of the picture-item is dependent on the image
+        // I recommend using imagesloaded to determine when an image is loaded
+        // but that doesn't support IE7
+        listenHackathons = function () {
+            let debouncedLayout = $.throttle(300, function () {
+                shuffler.update();
+            });
+
+            // Get all images inside shuffle
+            $grid.find('img').each(function () {
+                let proxyImage;
+
+                // Image already loaded
+                if (this.complete && this.naturalWidth !== undefined) {
+                    return;
+                }
+
+                // If none of the checks above matched, simulate loading on detached element.
+                proxyImage = new Image();
+                $(proxyImage).on('load', function () {
+                    $(this).off('load');
+                    debouncedLayout();
+                });
+
+                proxyImage.src = this.src;
+            });
+
+            // Because this method doesn't seem to be perfect.
+            setTimeout(function () {
+                debouncedLayout();
+            }, 500);
+        };
+
+    return {
+        init: init
+    };
+}(jQuery));
+
+$(document).ready(function () {
+    shufflemeHackathons.init(function () {
+        $('#about-hackathons a[data-group="eoshk"]').click();
+    }); //filter portfolio
+});
--- js/about-portfolio.js
+++ js/about-portfolio.js
@@ -1,15 +1,15 @@
-var filtersChosen = [];
-var shuffleme = (function ($) {
+var portfolioFiltersChosen = [];
+var shufflemePortfolio = (function ($) {
     'use strict';
-    var $grid = $('#grid'), //locate what we want to sort
-        $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
+    let $grid = $('#grid-portfolio'), //locate what we want to sort
+        $filterOptions = $('#about-portfolio .portfolio-sorting li'),  //locate the filter categories
         $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
         shuffler,
         init = function (onInit) {
             // None of these need to be executed synchronously
             setTimeout(function () {
-                listen();
-                setupFilters();
+                //listenPortfolioFilters();
+                setupPortfolioFilters();
                 onInit();
             }, 100);
 
@@ -18,36 +18,34 @@ var shuffleme = (function ($) {
                 itemSelector: '[class*="col-"][data-groups]',
                 sizer: $sizer[0]
             });
-        },
+        };
 
         // Set up button clicks
-        setupFilters = function () {
-            var $btns = $filterOptions.children();
+        var setupPortfolioFilters = function () {
+            let $btns = $filterOptions.children();
             $btns.on('click', function (e) {
                 e.preventDefault();
-                var $this = $(this),
+                let $this = $(this),
                     group = $this.data('group');
-
 
                 console.log("group " + group);
 
                 if ($this.hasClass('active')) {
                     // Hide current label, show current label in title
-                    filtersChosen = filtersChosen.filter(function (value, index, arr) {
-                        if (group === value)
-                            return false;
+                    portfolioFiltersChosen = portfolioFiltersChosen.filter(function (value) {
+                        return group !== value;
                     });
                     $this.removeClass('active');
                 } else {
                     $this.addClass('active');
                     if(Array.isArray(group)) {
-                        filtersChosen = filtersChosen.concat(group);
+                        portfolioFiltersChosen = portfolioFiltersChosen.concat(group);
                     } else {
-                        filtersChosen.push(group);
+                        portfolioFiltersChosen.push(group);
                     }
                 }
 
-                console.log("arrayss " + filtersChosen);
+                console.log("arrayss " + portfolioFiltersChosen);
 
                 /*
                  // Hide current label, show current label in title
@@ -58,9 +56,9 @@ var shuffleme = (function ($) {
                 // Filter elements
                 shuffler.filter((element, shuffle) => {
                     //console.log("elem: ", element);
-                    for (let j = 0; j < filtersChosen.length; j++) {
-                        console.log('j: ' + j + "; f: " + filtersChosen[j]);
-                        if ($(element).data('groups').indexOf(filtersChosen[j]) >= 0) return true;
+                    for (let j = 0; j < portfolioFiltersChosen.length; j++) {
+                        console.log('j: ' + j + "; f: " + portfolioFiltersChosen[j]);
+                        if ($(element).data('groups').indexOf(portfolioFiltersChosen[j]) >= 0) return true;
                     }
                     return false;
                 })
@@ -93,42 +91,42 @@ var shuffleme = (function ($) {
             });
 
             $btns = null;
-        },
+        };
 
         // Re layout shuffle when images load. This is only needed
         // below 768 pixels because the .picture-item height is auto and therefore
         // the height of the picture-item is dependent on the image
         // I recommend using imagesloaded to determine when an image is loaded
         // but that doesn't support IE7
-        listen = function () {
-            var debouncedLayout = $.throttle(300, function () {
-                shuffler.update();
-            });
-
-            // Get all images inside shuffle
-            $grid.find('img').each(function () {
-                var proxyImage;
-
-                // Image already loaded
-                if (this.complete && this.naturalWidth !== undefined) {
-                    return;
-                }
-
-                // If none of the checks above matched, simulate loading on detached element.
-                proxyImage = new Image();
-                $(proxyImage).on('load', function () {
-                    $(this).off('load');
-                    debouncedLayout();
-                });
-
-                proxyImage.src = this.src;
-            });
-
-            // Because this method doesn't seem to be perfect.
-            setTimeout(function () {
-                debouncedLayout();
-            }, 500);
-        };
+        // listenPortfolio = function () {
+        //     let debouncedLayout = $.throttle(300, function () {
+        //         shuffler.update();
+        //     });
+        //
+        //     // Get all images inside shuffle
+        //     $grid.find('img').each(function () {
+        //         var proxyImage;
+        //
+        //         // Image already loaded
+        //         if (this.complete && this.naturalWidth !== undefined) {
+        //             return;
+        //         }
+        //
+        //         // If none of the checks above matched, simulate loading on detached element.
+        //         proxyImage = new Image();
+        //         $(proxyImage).on('load', function () {
+        //             $(this).off('load');
+        //             debouncedLayout();
+        //         });
+        //
+        //         proxyImage.src = this.src;
+        //     });
+        //
+        //     // Because this method doesn't seem to be perfect.
+        //     setTimeout(function () {
+        //         debouncedLayout();
+        //     }, 500);
+        // };
 
     return {
         init: init
@@ -138,7 +136,7 @@ var shuffleme = (function ($) {
 //Set up subheader
 function displaySubHeader() {
     $(window).scroll(function () {
-        if (($(window).scrollTop() + 20 >= $('#about').offset().top) && ($(window).scrollTop() + 100 < $('#team').offset().top)) {
+        if (($(window).scrollTop() + 20 >= $('#about-portfolio').offset().top) && ($(window).scrollTop() + 100 < $('#team').offset().top)) {
             $(".subheader-section").css({
                 "display": "block"
             });
@@ -163,7 +161,7 @@ function displaySubHeader() {
 
 $(document).ready(function () {
     displaySubHeader();
-    shuffleme.init(function () {
-        $('a[data-group="eoshk"]').click();
+    shufflemePortfolio.init(function () {
+        //$('a[data-group="eoshk"]').click();
     }); //filter portfolio
 });
