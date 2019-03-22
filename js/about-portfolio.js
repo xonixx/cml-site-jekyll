acce1cf19005a842efa94a745ff1eb6d97af8f1d
AnnaLikhachova <a.cannsh@gmail.com>
2019-03-22 21:58:15 +0200

    Updated about section.

--- js/about-portfolio.js
+++ js/about-portfolio.js
@@ -1,7 +1,7 @@
 var portfolioFiltersChosen = [];
 var shufflemePortfolio = (function ($) {
-      'use strict';
-      let $grid = $('#grid-portfolio'), //locate what we want to sort
+    'use strict';
+    let $grid = $('#grid-portfolio'), //locate what we want to sort
         $filterOptions = $('#about-portfolio .portfolio-sorting li'),  //locate the filter categories
         $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
         shuffler,
@@ -9,7 +9,6 @@ var shufflemePortfolio = (function ($) {
             // None of these need to be executed synchronously
             setTimeout(function () {
                 //listenPortfolioFilters();
-                showPortfolioContent();
                 setupPortfolioFilters();
                 onInit();
             }, 100);
@@ -21,41 +20,31 @@ var shufflemePortfolio = (function ($) {
             });
         };
 
+        // Set up button clicks
+        var setupPortfolioFilters = function () {
+            let $btns = $filterOptions.children();
+            $btns.on('click', function (e) {
+                e.preventDefault();
+                let $this = $(this),
+                    group = $this.data('group');
+                console.log("this element of active class " + $this);
 
-      // Set up button clicks
-      var setupPortfolioFilters = function () {
-          let $btns = $filterOptions.children();
-          $btns.on('click', function (e) {
-              e.preventDefault();
-              let $this = $(this),
-                group = $this.data('group');
+                console.log("group " + group);
 
-              console.log($this);
-
-              if ($this.hasClass('active')) {
-                  if (Array.isArray(group)) return;
-                  // Hide current label, show current label in title
-                  portfolioFiltersChosen = portfolioFiltersChosen.filter(function (value) {
-                      return group !== value;
-                  });
-                  $this.removeClass('active');
-                  if ($(".portfolio-title a.active").length === 0) {
-                      $("#filter-all").click();
-                  }
-
-              } else {
-                  if (Array.isArray(group)) {
-                      portfolioFiltersChosen = group;
-                      $(".portfolio-title a").removeClass('active');
-                  } else {
-                      if ($("#filter-all").hasClass("active")) {
-                          $("#filter-all").removeClass("active");
-                          portfolioFiltersChosen = [];
-                      }
-                      portfolioFiltersChosen.push(group);
-                  }
-                  $this.addClass('active');
-              }
+                if ($this.hasClass('active')) {
+                    // Hide current label, show current label in title
+                    portfolioFiltersChosen = portfolioFiltersChosen.filter(function (value) {
+                        return group !== value;
+                    });
+                    $this.removeClass('active');
+                } else {
+                    $this.addClass('active');
+                    if(Array.isArray(group)) {
+                        portfolioFiltersChosen = portfolioFiltersChosen.concat(group);
+                    } else {
+                        portfolioFiltersChosen.push(group);
+                    }
+                }
 
                 //console.log("arrayss " + portfolioFiltersChosen);
                 let portfolioFilterChosenConcat = portfolioFiltersChosen.join(", ");
@@ -72,109 +61,94 @@ var shufflemePortfolio = (function ($) {
                     "text-transform": "capitalize"
                 });
 
-              /*
-               // Hide current label, show current label in title
-               $('.portfolio-sorting li a').removeClass('active');
+                /*
+                 // Hide current label, show current label in title
+                 $('.portfolio-sorting li a').removeClass('active');
 
-               $this.addClass('active');
-              */
-              // Filter elements
-              shuffler.filter((element, shuffle) => {
-                  //console.log("elem: ", element);
-                  for (let j = 0; j < portfolioFiltersChosen.length; j++) {
-                      console.log('j: ' + j + "; f: " + portfolioFiltersChosen[j]);
-                      if ($(element).data('groups').indexOf(portfolioFiltersChosen[j]) >= 0) return true;
-                  }
-                  return false;
-              })
+                 $this.addClass('active');
+                */
+                // Filter elements
+                shuffler.filter((element, shuffle) => {
+                    //console.log("elem: ", element);
+                    for (let j = 0; j < portfolioFiltersChosen.length; j++) {
+                        console.log('j: ' + j + "; f: " + portfolioFiltersChosen[j]);
+                        if ($(element).data('groups').indexOf(portfolioFiltersChosen[j]) >= 0) return true;
+                    }
+                    return false;
+                })
 
-              // $grid.find('li').filter(function (i, e) {
-              //     var $img = $(this).find('img');
-              //     $img.removeAttr('data-bp');
-              //     let groups = $(e).data('groups');
-              //     console.log("groups lalala " + groups);
-              //     if (!groups)
-              //         return false;
-              //
-              //     for(let j = 0; j < filtersChosen.length; j++) {
-              //         console.log('j: '+j+"; f: "+ filtersChosen[j]);
-              //         if(groups.indexOf(filtersChosen[j]) === -1) return false;
-              //     }
-              //     // if ($img.length) {
-              //     //     $img.attr('data-bp', $img.attr('src').replace('Converted120', 'Converted300'));
-              //     // }
-              //     return true;
-              // }).find('a')
-              //     .off('click.bigpicture')
-              //     .on('click.bigpicture', function (e) {
-              //         e.preventDefault();
-              //         BigPicture({
-              //             el: e.target,
-              //             gallery: '#grid'
-              //         })
-              //     });
-          });
+                // $grid.find('li').filter(function (i, e) {
+                //     var $img = $(this).find('img');
+                //     $img.removeAttr('data-bp');
+                //     let groups = $(e).data('groups');
+                //     console.log("groups lalala " + groups);
+                //     if (!groups)
+                //         return false;
+                //
+                //     for(let j = 0; j < filtersChosen.length; j++) {
+                //         console.log('j: '+j+"; f: "+ filtersChosen[j]);
+                //         if(groups.indexOf(filtersChosen[j]) === -1) return false;
+                //     }
+                //     // if ($img.length) {
+                //     //     $img.attr('data-bp', $img.attr('src').replace('Converted120', 'Converted300'));
+                //     // }
+                //     return true;
+                // }).find('a')
+                //     .off('click.bigpicture')
+                //     .on('click.bigpicture', function (e) {
+                //         e.preventDefault();
+                //         BigPicture({
+                //             el: e.target,
+                //             gallery: '#grid'
+                //         })
+                //     });
+            });
 
-          $btns = null;
-      };
+            $btns = null;
+        };
 
-      var showPortfolioContent = function () {
-          $(".projects").on('click', function (e) {
-              console.log("e " + this);
 
-              let id = this.id;
-              console.log("id " + $("." + id).hasClass("hidden"));
-              if ($("." + id).hasClass("hidden")) {
-                  $("." + id).removeClass("hidden");
-                  $(this).closest("li").css({"width": "100%"});
-              } else {
-                  $("." + id).addClass("hidden");
-                  $(this).closest("li").css({"width": ""});
-              }
-              shuffler.update();
-          });
-      }
 
-      // Re layout shuffle when images load. This is only needed
-      // below 768 pixels because the .picture-item height is auto and therefore
-      // the height of the picture-item is dependent on the image
-      // I recommend using imagesloaded to determine when an image is loaded
-      // but that doesn't support IE7
-      // listenPortfolio = function () {
-      //     let debouncedLayout = $.throttle(300, function () {
-      //         shuffler.update();
-      //     });
-      //
-      //     // Get all images inside shuffle
-      //     $grid.find('img').each(function () {
-      //         var proxyImage;
-      //
-      //         // Image already loaded
-      //         if (this.complete && this.naturalWidth !== undefined) {
-      //             return;
-      //         }
-      //
-      //         // If none of the checks above matched, simulate loading on detached element.
-      //         proxyImage = new Image();
-      //         $(proxyImage).on('load', function () {
-      //             $(this).off('load');
-      //             debouncedLayout();
-      //         });
-      //
-      //         proxyImage.src = this.src;
-      //     });
-      //
-      //     // Because this method doesn't seem to be perfect.
-      //     setTimeout(function () {
-      //         debouncedLayout();
-      //     }, 500);
-      // };
 
-      return {
-          init: init
-      };
-  }(jQuery)
-);
+        // Re layout shuffle when images load. This is only needed
+        // below 768 pixels because the .picture-item height is auto and therefore
+        // the height of the picture-item is dependent on the image
+        // I recommend using imagesloaded to determine when an image is loaded
+        // but that doesn't support IE7
+        // listenPortfolio = function () {
+        //     let debouncedLayout = $.throttle(300, function () {
+        //         shuffler.update();
+        //     });
+        //
+        //     // Get all images inside shuffle
+        //     $grid.find('img').each(function () {
+        //         var proxyImage;
+        //
+        //         // Image already loaded
+        //         if (this.complete && this.naturalWidth !== undefined) {
+        //             return;
+        //         }
+        //
+        //         // If none of the checks above matched, simulate loading on detached element.
+        //         proxyImage = new Image();
+        //         $(proxyImage).on('load', function () {
+        //             $(this).off('load');
+        //             debouncedLayout();
+        //         });
+        //
+        //         proxyImage.src = this.src;
+        //     });
+        //
+        //     // Because this method doesn't seem to be perfect.
+        //     setTimeout(function () {
+        //         debouncedLayout();
+        //     }, 500);
+        // };
+
+    return {
+        init: init
+    };
+}(jQuery));
 
 /*
 //Set up subheader
@@ -197,27 +171,54 @@ function displaySubHeader() {
                 "padding-bottom": "10px 0"
             });
         }
+
+
+
     });
 }
 */
 
-//Show content for hportfolio section
-// function showPortfolioContent() {
-//
-//     $(".projects").on('click', function (e) {
-//         console.log("e " + this);
-//
-//         let id = this.id;
-//         console.log("id " + id);
-//         if ($("." + id).hasClass("hidden")) {
-//             $("." + id).removeClass("hidden");
-//             $(this).closest("li").css({"width": "100%"});
-//         } else {
-//             $("." + id).addClass("hidden");
-//             $(this).closest("li").css({"width": "33.3%"});
-//         }
-//     });
-// }
+//Show content for portfolio section
+function showPortfolioContent() {
+
+    $(".projects").on('click',function () {
+        console.log("e "+this);
+        let id = this.id;
+        console.log("id "+id);
+        if($("." + id).css("display") === "none" ) {
+            $("." + id).css({"display": "flex"});
+        } else {
+            $("." + id).css({"display": "none"});
+        }
+    });
+}
+
+//Show content for filter for mobile version
+function showFilterContent() {
+
+    /*
+    $('#about-portfolio .portfolio-sorting li a').on('click',function () {
+        var new_project = this.innerText;
+        if($(".portfolio-display-mobile-list li").length <=4 && !this.classList.contains('active')){
+        $(".portfolio-display-mobile-list li:eq(1)").after('<li class="portfolio-display-mobile-list-hidden">'+new_project+'</li>');
+        } else if($(".portfolio-display-mobile-list li").length === 5){
+            $(".portfolio-display-mobile-list li:eq(1)").after('<li class="portfolio-display-mobile-list-hidden">'+new_project+'</li>');
+            $(".portfolio-display-mobile-list-hidden").css({
+                "display": "none"
+            });
+            $(".portfolio-display-mobile-list li:eq(1)").after('<li>...</li>');
+        } else if(this.classList.contains('active')){
+           $(".portfolio-display-mobile-list li").parentNode.removeChild(this);
+        } else{
+            $(".portfolio-display-mobile-list li:eq(1)").after('<li class="portfolio-display-mobile-list-hidden">'+new_project+'</li>');
+            $(".portfolio-display-mobile-list-hidden").css({
+                "display": "none"
+            });
+        }
+    });
+    */
+}
+
 
 //Set up subheader for portfolio section
 function displaySubHeader() {
@@ -260,8 +261,9 @@ function displaySubHeader() {
 }
 
 $(document).ready(function () {
+    showPortfolioContent();
     displaySubHeader();
     shufflemePortfolio.init(function () {
-        $("#filter-all").click();
+        //$('a[data-group="eoshk"]').click();
     }); //filter portfolio
 });
