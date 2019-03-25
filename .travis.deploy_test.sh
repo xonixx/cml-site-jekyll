890ce78b383a15c94c8262222c04d595b881a179
xonix <xonixx@gmail.com>
2019-03-25 20:38:02 +0200

    change deployment config to deploy to next.cmlteam.com

--- .travis.deploy_test.sh
+++ .travis.deploy_test.sh
@@ -4,6 +4,11 @@ set -v
 
 ssh -i /tmp/deploy_rsa apps1@prod.cmlteam.com "
     (echo \"\$(date) www.cmlteam.com \$(pwd)\" ;
-    rm -rf www.cmlteam.com/test ;
-    mkdir www.cmlteam.com/test ) >> /tmp/.deploy 2>&1"
+    rm -rf www.cmlteam.com/test                ;
+    mkdir www.cmlteam.com/test                 ;
+    cd www.cmlteam.com/test                    ;
+    echo 'User-agent: *' > robots.txt          ;
+    echo 'Disallow: /'  >> robots.txt          ;
+    echo                >> robots.txt          ;
+    ) >> /tmp/.deploy 2>&1"
 cd _site ; scp -i /tmp/deploy_rsa -C -r * apps1@prod.cmlteam.com:~/www.cmlteam.com/test
