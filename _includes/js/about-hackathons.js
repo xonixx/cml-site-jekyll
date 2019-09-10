11604ebad526b44cde12bad11cb4c88d631ad57d
evRusin <evrusin@gmail.com>
2019-09-10 21:46:40 +0300

    CML-86: let added

--- _includes/js/about-hackathons.js
+++ _includes/js/about-hackathons.js
@@ -1,27 +1,27 @@
-var shufflemeHackathons = (function($) {
+var shufflemeHackathons = (function ($) {
   "use strict";
-  let $grid = $("#about-hackathons #grid"), //locate what we want to sort
-    $filterOptions = $("#about-hackathons .portfolio-sorting li"), //locate the filter categories
-    $sizer = $grid.find(".shuffle_sizer"), //sizer stores the size of the items
-    shuffler,
-    init = function(onInit) {
-      // None of these need to be executed synchronously
-      setTimeout(function() {
-        setupHackathonsFilters();
-        onInit();
-      }, 100);
+  let $grid = $("#about-hackathons #grid"); //locate what we want to sort
+  let $filterOptions = $("#about-hackathons .portfolio-sorting li"); //locate the filter categories
+  let $sizer = $grid.find(".shuffle_sizer"); //sizer stores the size of the items
+  let shuffler;
+  let init = function (onInit) {
+    // None of these need to be executed synchronously
+    setTimeout(function () {
+      setupHackathonsFilters();
+      onInit();
+    }, 100);
 
-      // instantiate the plugin
-      shuffler = new Shuffle($grid[0], {
-        itemSelector: '[class*="col-"][data-groups]',
-        sizer: $sizer[0]
-      });
-    };
+    // instantiate the plugin
+    shuffler = new Shuffle($grid[0], {
+      itemSelector: '[class*="col-"][data-groups]',
+      sizer: $sizer[0]
+    });
+  };
 
   // Set up button click
-  let setupHackathonsFilters = function() {
+  let setupHackathonsFilters = function () {
       let $btns = $filterOptions.children();
-      $btns.on("click", function(e) {
+      $btns.on("click", function (e) {
         e.preventDefault();
         let $this = $(this),
           group = $this.data("group");
@@ -41,7 +41,7 @@ var shufflemeHackathons = (function($) {
 
         $grid
           .find("li")
-          .filter(function(i, e) {
+          .filter(function (i, e) {
             let $img = $(this).find("img");
             $img.removeAttr("data-bp");
             let groups = $(e).data("groups");
@@ -57,7 +57,7 @@ var shufflemeHackathons = (function($) {
           })
           .find(".portfolio-item a")
           .off("click.bigpicture")
-          .on("click.bigpicture", function(e) {
+          .on("click.bigpicture", function (e) {
             e.preventDefault();
             BigPicture({
               el: e.target,
@@ -67,29 +67,29 @@ var shufflemeHackathons = (function($) {
       });
 
       $btns = null;
-    },
+    }
     // Re layout shuffle when images load. This is only needed
     // below 768 pixels because the .picture-item height is auto and therefore
     // the height of the picture-item is dependent on the image
     // I recommend using imagesloaded to determine when an image is loaded
     // but that doesn't support IE7
-    loadImages = function(insideGroup) {
-      let debouncedLayout = $.throttle(300, function() {
+    let loadImages = function (insideGroup) {
+      let debouncedLayout = $.throttle(300, function () {
         shuffler.update();
       });
 
       // Get all images inside shuffle selected group
       $grid
         .find("li")
-        .filter(function(i, e) {
+        .filter(function (i, e) {
           let groups = $(e).data("groups");
           if (!groups) return false;
           return groups.indexOf(insideGroup) >= 0;
         })
         .find("img")
-        .each(function() {
+        .each(function () {
           // If none of the checks above matched, simulate loading on detached element.
-          $(this).on("load", function() {
+          $(this).on("load", function () {
             $(this).off("load");
             debouncedLayout();
           });
@@ -98,7 +98,7 @@ var shufflemeHackathons = (function($) {
         });
 
       // Because this method doesn't seem to be perfect.
-      setTimeout(function() {
+      setTimeout(function () {
         debouncedLayout();
       }, 500);
     };
@@ -108,8 +108,8 @@ var shufflemeHackathons = (function($) {
   };
 })(jQuery);
 
-$(function() {
-  $("#about-hackathons .portfolio-display-mobile-list").on("click", function() {
+$(function () {
+  $("#about-hackathons .portfolio-display-mobile-list").on("click", function () {
     console.log("Clicked on", this);
     $("#about-hackathons .portfolio-sorting").toggleClass("mobile-hidden");
     $("#about-hackathons .portfolio-display-mobile-element").toggleClass("selected");
@@ -120,7 +120,7 @@ $(function() {
       .find(".container")
       .show();
 
-    shufflemeHackathons.init(function() {
+    shufflemeHackathons.init(function () {
       $('#about-hackathons a[data-group="EOS HK"]').click();
     });
   });
