687e38f4cde275eab14db295228e32878dbb008c
NineKFlames <cheberiak.av@gmail.com>
2019-11-21 14:14:14 +0200

    CML-126: Refactor Travis deployment to deploy master to TEST and stable to PROD

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+ENV_SPECIFIC_CONFIG_FILE="$1"
+
+if [ ! -e "$ENV_SPECIFIC_CONFIG_FILE" ]
+then
+  echo "Please provide an existing environment-specific file!"
+  echo "Received: \"$ENV_SPECIFIC_CONFIG_FILE\""
+  echo
+  exit 1
+fi
+
 set -v
 
-bundle exec jekyll build
+bundle exec jekyll build --config=./_config.yml,"$ENV_SPECIFIC_CONFIG_FILE"
 FILES_REGEX=".*\.\(html\|css\|js\|mp4\|png\|ico\|jpeg\|jpg\|svg\|pdf\|php\)"
 find _site -regextype sed -regex "${FILES_REGEX}" -print0 | xargs -0 gzip -9 -kv
 #mv _site site # looks like deploy phase doesn't like underscores
