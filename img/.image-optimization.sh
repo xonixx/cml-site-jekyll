de3314910a9459c3f4102a88fe3792f76394fe68
NineKFlames <cheberiak.av@gmail.com>
2019-11-26 19:14:54 +0200

    CML-128: Add capitalized picture formats to image optimization shell

--- img/.image-optimization.sh
+++ img/.image-optimization.sh
@@ -10,7 +10,7 @@ find "$SOURCE_FOLDER" -type d ! -wholename "$SOURCE_FOLDER" -print0 | # list all
   xargs -i mkdir -p "$IMG_FOLDER"/{} # attempt to create all folders, if not present
 
 # Copy over all images with conversion to WebP
-find "$SOURCE_FOLDER" -regextype sed -regex ".*\.\(jpg\|png\|jpeg\)" -print0 | # list all images in source dir as abspaths
+find "$SOURCE_FOLDER" -regextype sed -regex ".*\.\(jpg\|JPG\|png\|PNG\|jpeg\|JPEG\)" -print0 | # list all images in source dir as abspaths
   xargs --null -L 1 -i realpath --relative-to="$SOURCE_FOLDER" {} | # turn all abspaths to relpaths
   # feed all relpaths to image-magick
   xargs -L 1 -i \
