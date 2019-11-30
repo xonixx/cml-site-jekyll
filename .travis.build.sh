5d820913e9029d459d87e68e137ce1db1442cc99
xonix <xonixx@gmail.com>
2019-11-30 17:33:01 +0200

    fix

--- .travis.build.sh
+++ .travis.build.sh
@@ -13,8 +13,6 @@ fi
 set -v
 
 bundle exec jekyll build --config=./_config.yml,"$ENV_SPECIFIC_CONFIG_FILE"
-#FILES_REGEX=".*\.\(html\|css\|js\|php\|ttf|\woff\|woff2\|eot\|svg\)"
-#find _site -regextype sed -regex "${FILES_REGEX}" -print0 | xargs -0 gzip -9 -kv
 #mv _site site # looks like deploy phase doesn't like underscores
 #echo www.cmlteam.com > site/CNAME # custom domain
 rm -f _site/*.sh _site/*.enc
