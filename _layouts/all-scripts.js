59f2a37e448e55a757fafc2226d0344ac8d35f61
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-04-26 14:29:12 +0300

    fix: fixed bloced by addblock intro anim

--- _layouts/all-scripts.js
+++ _layouts/all-scripts.js
@@ -5,12 +5,12 @@
 // {% include js/introAnim.2.0.0.js %};
 {% include js/typed.1.1.7.min.js %};
 
-{% include js/cbpAnimatedHeader.js %};
+// {% include js/cbpAnimatedHeader.js %};
 {% include js/about-hackathons.js %};
 {% include js/about-portfolio.js %};
 {% include js/about.js %};
 {% include js/contact_me.js %};
-{% include js/cml.js %};
+// {% include js/cml.js %};
 {% include js/service.js %};
-{% include js/events.js %};
+// {% include js/events.js %};
 {% include js/teamPart.js %};
