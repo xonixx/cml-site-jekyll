76cc6713ad0801d8987eddda2a45c33d4561df6d
Oleksii Shkulipa <oleksii.shkulipa@gmail.com>
2022-04-26 15:08:43 +0300

    fix: fixed blocked by adblock intro anim

--- _layouts/all-scripts.js
+++ _layouts/all-scripts.js
@@ -1,17 +1,2 @@
-{% include js/jquery.easing.min.js %};
-{% include js/jquery.inview.min.js %};
-{% include js/classie.js %};
-{% include js/jqBootstrapValidation.js %};
 
-{% include js/typed.1.1.7.min.js %};
-
-{% include js/cbpAnimatedHeader.js %};
-{% include js/about-hackathons.js %};
-{% include js/about-portfolio.js %};
-{% include js/about.js %};
-{% include js/contact_me.js %};
-{% include js/cml.js %};
-{% include js/service.js %};
-{% include js/events.js %};
-{% include js/teamPart.js %};
 console.log(1);
